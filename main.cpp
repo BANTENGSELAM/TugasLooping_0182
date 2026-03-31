44aee63d304cd25fb3f635a2b65302f5a8fb3cbf
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-31 11:40:44 +0700

    bil prima

--- main.cpp
+++ main.cpp
@@ -11,3 +11,17 @@ void inputAngka() {
     cout << "Masukkan angka: ";
     cin >> angka;
 }
+
+//untuk cek bi;l prima
+bool isPrima(int n) {
+    if (n <= 1) return false;
+
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
